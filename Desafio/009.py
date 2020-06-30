7646014c46f1d3885ed58ab13d7c6375e3b9b5bc
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-06-30 14:00:14 -0300

    Desafio tabuada
    Le um numero e mostra sua tabuada na tela.

---
+++ Desafio/009.py
@@ -0,0 +1,14 @@
+n0 = int(input('Digite um numero '))
+n1 = n0*1
+n2 = n0*2
+n3 = n0*3
+n4 = n0*4
+n5 = n0*5
+n6 = n0*6
+n7 = n0*7
+n8 = n0*8
+n9 = n0*9
+n10 = n0*10
+print('{} x 1 = {} | {} x 2 = {}\n{} x 3 = {} | {} x 4 = {}'.format(n0, n1, n0, n2, n0, n3, n0, n4))
+print('{} x 5 = {} | {} x 6 = {}\n{} x 7 = {} | {} x 8 = {}'.format(n0, n5, n0, n6, n0, n7, n0, n8))
+print('{} x 9 = {} | {} x 10 = {}'.format(n0, n9, n0, n10))

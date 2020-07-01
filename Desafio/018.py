ce6ea953dfa4ee6ae6f9aa82266ba867e37343bc
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-01 04:49:03 -0300

    Desafio angulo
    recebe um angulo e mostra o valor do seno, cosseno e tangente

---
+++ Desafio/018.py
@@ -0,0 +1,8 @@
+import math
+n0 = float(input('Digite o angulo: '))
+rad = math.radians(n0)
+sen = math.sin(rad)
+cos = math.cos(rad)
+tan = math.tan(rad)
+print('O angulo de {} tem o seno {:.2f}\nO angulo {} tem o cosseno {:.2f}\n O angulo de {} tem a tangente {:.2f}'.format(
+    n0, sen, n0, cos, n0, tan))

ef468f9ab6fd9686f499ea5db5101ebdcd546d33
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-06-30 21:23:22 -0300

    Desafio dolares
    Quantos dolares podem ser comprados

---
+++ Desafio/010.py
@@ -0,0 +1,5 @@
+n0 = float(input('Quanto dinheiro em reais você tem sua carteira '))
+n1 = 3.27
+n2 = n0/n1
+print('A cotação esta {}'.format(n1))
+print('Você pode comprar {:.2f} dólares'.format(n2))

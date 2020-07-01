0891e2c816de0f9538912d94ec235c1912e8a5c8
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-06-30 21:38:07 -0300

    Desafio area e tinta
    Quantos litros de tinta sao gastos para pintar uma parede

---
+++ Desafio/011.py
@@ -0,0 +1,4 @@
+n0 = float(input('Em metros qual a altura da parede? '))
+n1 = float(input('Em metros qual a largura da parede? '))
+n3 = n1*n0
+print('Você gastara {} litros de tinta na pintura da parede'.format(n3/2))

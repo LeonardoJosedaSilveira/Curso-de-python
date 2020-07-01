4590d77f4a7710165909c1ce5b5ec5f92db03e98
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-01 12:29:39 -0300

    Feito posterior a aula
    Correção na queba de linha do codigo

--- Desafio/018.py
+++ Desafio/018.py
@@ -4,5 +4,4 @@ rad = math.radians(n0)
 sen = math.sin(rad)
 cos = math.cos(rad)
 tan = math.tan(rad)
-print('O angulo de {} tem o seno {:.2f}\nO angulo {} tem o cosseno {:.2f}\n O angulo de {} tem a tangente {:.2f}'.format(
-    n0, sen, n0, cos, n0, tan))
+print('O angulo de {} tem o seno {:.2f}\nO angulo {} tem o cosseno {:.2f}\n O angulo de {} tem a tangente {:.2f}'.format(n0, sen, n0, cos, n0, tan))

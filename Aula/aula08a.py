58ea6cebf1f5e23978a3cb93a728882e894a7895
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-01 03:09:25 -0300

    Feito pos aula
    atualizao para exemplo

--- Aula/aula08a.py
+++ Aula/aula08a.py
@@ -1,2 +1,5 @@
-import emoji
+import emoji, math
 print(emoji.emojize("Ola mundo :earth_americas:", use_aliases=True))
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A raiz de {} é igual a {}'.format(num, raiz))

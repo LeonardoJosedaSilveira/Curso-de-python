14d37423ee6c95537e759584e1b159086b5f8d3a
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-03 01:39:32 -0300

    Desafio separe um numero
    Faça um progra que leia um numero de 0 a 9999 e mostre na tela os digitos separados

---
+++ Desafio/023.py
@@ -0,0 +1,5 @@
+numero = str(input('Digite um numero de 0 a 9999: '))
+print('unidade: {}'.format(numero[3]))
+print('dezena: {}'.format(numero[2]))
+print('Centena: {}'.format(numero[1]))
+print('milhar: {}'.format(numero[0]))

132b9bb84f031b41d119ff7706360a323e6e7678
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-02 22:10:13 -0300

    Create aula09a.py
    treinando aula 09

---
+++ Aula/aula09a.py
@@ -0,0 +1,34 @@
+frase = '  Curso em Video Python  '
+#        012345678901234567890
+#print(frase[3])
+#print(frase[13:])
+#print(frase[6::4])
+'''print("""asdfadsfasdfasdfasdfasdfasdfas
+      dfasdfasdfasdfadsfasdfasdfasdfasdf
+      asdfasdfasdfasdfasdfasdfasdfasdfasdfa
+      sdfasdfasdfasdfasdfasdfasdfasdfas)""")'''
+#contar letras escolhidas
+#print('Na frase sao {} letras O minusculas.'.format(frase.count('o')))
+# passa para maiusculas
+#print(frase.upper())
+#conta total de caracteres na frase
+#print(len(frase))
+#remove espaços na antes e depois da frase
+#print(frase.strip())
+#replace altera a frase
+#print(frase.replace('Python', 'android'))
+#conferir conteudo de frase
+#print('Curso' in frase)
+#pesquisa no conteudo da frase e retorna posição inicial
+#print(frase.find('Video'))
+#troca frase para minuscula
+#print(frase.lower())
+#split divide a frase em palavras o divisor é o espaço entre elas
+#print(frase.split())
+#a frase dividida se torna uma lista com palavras posicionadas a partir do 0
+dividida = (frase.split())
+print(dividida[2])
+#para selecinar a letra dentro da palavra seleciona se usa [] []
+#print(dividida[2][3])
+#join junta a frase divida ou com novas palavras
+print(''.join())

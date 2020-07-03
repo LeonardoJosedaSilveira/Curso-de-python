6080487ce57c504e3dbd7403dfc591e21efd0a58
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-02 22:50:59 -0300

    Update aula09a.py
    limpesa de comentarios

--- Aula/aula09a.py
+++ Aula/aula09a.py
@@ -1,34 +1,40 @@
 frase = '  Curso em Video Python  '
-#        012345678901234567890
-#print(frase[3])
-#print(frase[13:])
-#print(frase[6::4])
-'''print("""asdfadsfasdfasdfasdfasdfasdfas
+#        0123456789012345678901234
+print(frase[3])
+print(frase[13:])
+print(frase[6::4])
+# """ e ''' tambem servem para comentarios em blocos
+'''
+teste teste teste
+teste teste teste
+'''
+# """ server para textos maiores que uma linha assim como '''
+print("""asdfadsfasdfasdfasdfasdfasdfas
       dfasdfasdfasdfadsfasdfasdfasdfasdf
       asdfasdfasdfasdfasdfasdfasdfasdfasdfa
-      sdfasdfasdfasdfasdfasdfasdfasdfas)""")'''
+      sdfasdfasdfasdfasdfasdfasdfasdfas)""")
 #contar letras escolhidas
-#print('Na frase sao {} letras O minusculas.'.format(frase.count('o')))
+print('Na frase sao {} letras O minusculas.'.format(frase.count('o')))
 # passa para maiusculas
-#print(frase.upper())
+print(frase.upper())
 #conta total de caracteres na frase
-#print(len(frase))
+print(len(frase))
 #remove espaços na antes e depois da frase
-#print(frase.strip())
+print(frase.strip())
 #replace altera a frase
-#print(frase.replace('Python', 'android'))
+print(frase.replace('Python', 'android'))
 #conferir conteudo de frase
-#print('Curso' in frase)
+print('Curso' in frase)
 #pesquisa no conteudo da frase e retorna posição inicial
-#print(frase.find('Video'))
+print(frase.find('Video'))
 #troca frase para minuscula
-#print(frase.lower())
+print(frase.lower())
 #split divide a frase em palavras o divisor é o espaço entre elas
-#print(frase.split())
+print(frase.split())
 #a frase dividida se torna uma lista com palavras posicionadas a partir do 0
 dividida = (frase.split())
 print(dividida[2])
 #para selecinar a letra dentro da palavra seleciona se usa [] []
-#print(dividida[2][3])
+print(dividida[2][3])
 #join junta a frase divida ou com novas palavras
-print(''.join())
+print(' '.join(frase))

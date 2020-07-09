3289c7792c6869a2c3a6cefd174137c26930b3dc
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-09 15:56:06 -0300

    colocando cores

--- Desafio/003.py
+++ Desafio/003.py
@@ -1,4 +1,5 @@
 nun1 = int(input('Insira um numero :'))
 nun2 = int(input('insira outro numero :'))
 soma = nun1 + nun2
-print('A soma dos valores é {}'.format(soma))
+#print('A soma dos valores é {}'.format(soma))
+print('\033[1;32;40mA soma dos valores é{}\033[m'.format(soma))

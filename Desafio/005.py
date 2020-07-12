4a8d672043fc9b3c2c92e6404b30dd6e358121d3
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-11 23:20:01 -0300

    cores comentadas

--- Desafio/005.py
+++ Desafio/005.py
@@ -1,5 +1,5 @@
 n1 = int(input('Digite um valor '))
 n2 = n1-1
 n3 = n1+1
-print('\033[1;34;45mO antecessor é {} \033[me o sucessor {}'.format(n2, n3))
+#print('\033[1;34;45mO antecessor é {} \033[me o sucessor {}'.format(n2, n3))
 print('O antecessor é {} e o sucessor {}'.format(n2, n3))

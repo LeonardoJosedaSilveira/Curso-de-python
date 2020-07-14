c0bd74db53599996e92d787c61d95ba48ccb4bad
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-14 18:27:26 -0300

    cores

--- Desafio/007.py
+++ Desafio/007.py
@@ -2,3 +2,4 @@ n1 = int(input('Digite a primeira nota '))
 n2 = int(input('Digite a segunda nota '))
 n3 = (n1+n2)/2
 print('A media é {}'.format(n3))
+print('\033[2;39;45mA media é {}\033[m'.format(n3))

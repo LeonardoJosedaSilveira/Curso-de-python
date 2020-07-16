eb3fec63b1974d08048eb6030da8f095ea89bbfa
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-15 22:31:30 -0300

    cores

--- Desafio/008.py
+++ Desafio/008.py
@@ -2,3 +2,4 @@ n1 = float(input('Digite a quantidade de metros '))
 n2 = n1*100
 n3 = n1*1000
 print('São {:.0f} centímetros ou {:.0f} milímetros'.format(n2, n3))
+print('\033[1;37;42mSão {:.0f} centímetros ou {:.0f} milímetros\033[m'.format(n2, n3))

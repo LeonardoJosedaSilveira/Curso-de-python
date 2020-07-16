140e3299f8e4d2b98f2916fed3456822cb1f4bc0
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-16 19:06:46 -0300

    Update 009.py

--- Desafio/009.py
+++ Desafio/009.py
@@ -12,3 +12,4 @@ n10 = n0*10
 print('{} x 1 = {} | {} x 2 = {}\n{} x 3 = {} | {} x 4 = {}'.format(n0, n1, n0, n2, n0, n3, n0, n4))
 print('{} x 5 = {} | {} x 6 = {}\n{} x 7 = {} | {} x 8 = {}'.format(n0, n5, n0, n6, n0, n7, n0, n8))
 print('{} x 9 = {} | {} x 10 = {}'.format(n0, n9, n0, n10))
+print('\033[0;31;42mFim\033[m')

cab9ef49e82dc7ca77482a928ff2004f2313ba7f
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-18 15:54:25 -0300

    cores

--- Desafio/013.py
+++ Desafio/013.py
@@ -1,3 +1,4 @@
 n0 = float(input('Qual o salario do funcionario? '))
 n1 = (n0/100)*15
 print('O valor do salário com o aumento será {:.2f}'.format(n0+n1))
+print('\033[2;34;41mteste de cor\033[m')
--- Desafio/014.py
+++ Desafio/014.py
@@ -1,3 +1,4 @@
 c = float(input('Digite a temperatura em ºC: '))
 f = 9 * c / 5 + 32
 print('A temperatura de {} ºC corresponde a {} ºF!'.format(c, f))
+print('\033[2;35;42mcolocando cor\033[m')

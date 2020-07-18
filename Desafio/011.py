435a371c7070c81fbb11ee325446ddc2672924e9
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-18 15:09:56 -0300

    cores

--- Desafio/011.py
+++ Desafio/011.py
@@ -2,3 +2,4 @@ n0 = float(input('Em metros qual a altura da parede? '))
 n1 = float(input('Em metros qual a largura da parede? '))
 n3 = n1*n0
 print('Você gastara {} litros de tinta na pintura da parede'.format(n3/2))
+print('\033[0;36;47mesta é a cor\033[m')

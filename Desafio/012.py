f4fb7a6166036f9ee10e6c364c7b22e4f37e58e7
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-18 15:11:49 -0300

    cores

--- Desafio/012.py
+++ Desafio/012.py
@@ -1,3 +1,4 @@
 n0 = float(input('Qual o preço do produto? '))
 n1 = (n0/100)*5
 print('O produto com desconto fica {:.2f}'.format(n0-n1))
+print('\033[2;31;42mO produto com desconto fica \033[m{:.2f}'.format(n0-n1))

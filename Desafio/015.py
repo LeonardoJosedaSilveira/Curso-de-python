5a2af36e5bf806204a95a7767dc3434800fefd00
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-19 13:58:03 -0300

    cores e comentario

--- Desafio/015.py
+++ Desafio/015.py
@@ -2,4 +2,4 @@ dias = int(input('Quantos dias alugados? '))
 km = float(input('Quantos Km rodados? '))
 pago = dias * 60 + km * 0.15
 print('O total a pagar é de R${:.2f}'.format(pago))
-print('\033[1;34;43mteste O corinho\033[m')
+#print('\033[1;34;43mteste O corinho\033[m')
--- Desafio/016.py
+++ Desafio/016.py
@@ -1,2 +1,3 @@
 n0 = float(input('Digite um numero '))
 print('A parte inteira do número é {:.0f}'.format(n0 // 1))
+print('\033[1;35;41meste é um teste de cores\033[m')

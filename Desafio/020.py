7fc5df7578b7ab668f9db323043135ba9df2bb04
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-24 21:55:40 -0300

    cores

--- Desafio/020.py
+++ Desafio/020.py
@@ -1,7 +1,7 @@
 import random
-n0 = str(input('digite o nome do aluno: '))
-n1 = str(input('digite o nome do aluno: '))
-n2 = str(input('digite o nome do aluno: '))
+n0 = str(input('\033[02;31;40mdigite o nome do aluno:\033[m '))
+n1 = str(input('\033[01;31;40mdigite o nome do aluno:\033[m '))
+n2 = str(input('\033[03;31;40mdigite o nome do aluno:\033[m '))
 n3 = str(input('digite o nome do aluno: '))
 lista = [n0, n1, n2, n3]
 random.shuffle(lista)

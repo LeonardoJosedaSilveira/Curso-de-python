1565321809845fb3cfc1aab182d567ca68f0aa26
Leonardo Jose da Silveira <leonardojosedasilveira@gmail.com>
2020-07-25 13:45:41 -0300

    teste

--- Desafio/020.py
+++ Desafio/020.py
@@ -5,4 +5,4 @@ n2 = str(input('\033[03;31;40mdigite o nome do aluno:\033[m '))
 n3 = str(input('digite o nome do aluno: '))
 lista = [n0, n1, n2, n3]
 random.shuffle(lista)
-print('A ordem das apresentações será {}'.format(lista));
+print('A ordem das apresentações será {}'.format(lista));;;;;;
